d8e406bbaf2960a1ce347a0f8a3545423b546908
tanvirrandhawa <tanvirrandhawa@tanvirs-mbp.fios-router.home>
2021-10-02 11:21:13 -0400

    clear error

--- src/saturday/DemoSaturday.java
+++ src/saturday/DemoSaturday.java
@@ -4,7 +4,8 @@ public class DemoSaturday {
 	public static void main(String[] args) {
 		System.out.println("Saturday Demo");
 	}
-	public static void subNum(int a, int b)
+	public static void subNum(int a, int b) {
 		System.out.println(a-b);
 
+}
 }
