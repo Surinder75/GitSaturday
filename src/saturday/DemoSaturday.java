a795b2086f3ce82152adb36c4bc9b13acca69774
Surinder75 <91563453+Surinder75@users.noreply.github.com>
2021-10-02 10:59:41 -0400

    Update DemoSaturday.java
    added subNum method

--- src/saturday/DemoSaturday.java
+++ src/saturday/DemoSaturday.java
@@ -4,5 +4,7 @@ public class DemoSaturday {
 	public static void main(String[] args) {
 		System.out.println("Saturday Demo");
 	}
+	public static void subNum(int a, int b)
+		System.out.println(a-b);
 
 }
