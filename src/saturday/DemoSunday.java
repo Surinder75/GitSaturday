90a1823eacf7ceb1aa0f54212c36aa10d6ff5933
tanvirrandhawa <tanvirrandhawa@tanvirs-mbp.fios-router.home>
2021-10-02 11:19:43 -0400

    saturday changes

--- src/saturday/DemoSunday.java
+++ src/saturday/DemoSunday.java
@@ -3,6 +3,9 @@ package saturday;
 public class DemoSunday {
 	public static void main(String[] args) {
 		System.out.println("Sunday Demo");
+		System.out.println("Sunday");
 	}
-
+public static void addNum(int a ,int b) {
+	System.out.println(a+b);
+}
 }
